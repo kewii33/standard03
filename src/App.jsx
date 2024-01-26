ad917302144da3af197405221336c980e21c4488
kewii33 <kewiii071@gmail.com>
2024-01-26 16:20:21 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   const students = [
-    { name: "Alice", age: 17, grade: "A" },
-    { name: "Bob", age: 18, grade: "B" },
-    { name: "Charlie", age: 16, grade: "C" },
-    { name: "Diana", age: 19, grade: "D" },
+    { name: 'Alice', age: 17, grade: 'A' },
+    { name: 'Bob', age: 18, grade: 'B' },
+    { name: 'Charlie', age: 16, grade: 'C' },
+    { name: 'Diana', age: 19, grade: 'D' },
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((students) => students.age >= 18);
 
   return (
     <div>
@@ -17,6 +17,15 @@ function App() {
       <ul>
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
+        {filteredStudents.map((students) => (
+          <li
+            onClick={() =>
+              alert(`나이 : ${students.age}, 점수 : ${students.grade}`)
+            }
+          >
+            {students.name}
+          </li>
+        ))}
       </ul>
     </div>
   );
