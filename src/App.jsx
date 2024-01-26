e7fd8849d13f148a8edd6e5e36c9419033dbf367
kewii33 <kewiii071@gmail.com>
2024-01-26 17:28:03 +0900

    ex4

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
-import FilterButtons from "./components/FilterButtons";
-import StudentList from "./components/StudentList";
+import React, { useState } from 'react';
+import FilterButtons from './components/FilterButtons';
+import StudentList from './components/StudentList';
 
 function App() {
   const initialStudents = [
-    { name: "Alice", age: 17, grade: "A" },
-    { name: "Bob", age: 18, grade: "B" },
-    { name: "Charlie", age: 16, grade: "C" },
-    { name: "Diana", age: 19, grade: "D" },
+    { name: 'Alice', age: 17, grade: 'A' },
+    { name: 'Bob', age: 18, grade: 'B' },
+    { name: 'Charlie', age: 16, grade: 'C' },
+    { name: 'Diana', age: 19, grade: 'D' },
   ];
+
+  const filterByAge = (minAge) => {
+    const filterAge = initialStudents.filter(
+      (student) => student.age >= minAge
+    );
+    setFilteredStudents(filterAge);
+  };
+  const filterByGrade = (grade) => {
+    const filterGrade = initialStudents.filter(
+      (student) => student.grade === grade
+    );
+    setFilteredStudents(filterGrade);
+  };
+  const resetFilter = () => {
+    setFilteredStudents(initialStudents);
+  };
 
   const [filteredStudents, setFilteredStudents] = useState(initialStudents);
 
@@ -17,12 +33,16 @@ function App() {
       <h1>학생 목록</h1>
       {/* TODO: FilterButtons 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
       <FilterButtons
-      /* 필요한 props를 여기에 전달하세요. */
+        /* 필요한 props를 여기에 전달하세요. */
+        filterByAge={filterByAge}
+        filterByGrade={filterByGrade}
+        resetFilter={resetFilter}
       />
 
       {/* TODO: StudentList 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
       <StudentList
-      /* 필요한 props를 여기에 전달하세요. */
+        students={filteredStudents}
+        /* 필요한 props를 여기에 전달하세요. */
       />
     </div>
   );
